b3fbdcfb7bd006d45d243e67b66c6da3bf869b4c
ayushkr101 <ayushkr101@gmail.com>
2018-11-27 18:49:40 +0530

    layout2

--- plain.js
+++ plain.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-    $("a").addClass( 'btn btn-success' );
    
+    $("a").addClass( 'btn btn-success' );
+    
+    $("a").hover(function(){
+        
+        $(this).css("background-color", "green");
+        }, function(){
+        $(this).css("background-color", "#00b33c");
+        
+    });
+
 
 });
