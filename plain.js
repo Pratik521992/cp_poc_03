83726891f21119b6c7402f9393c2426b2af00051
Aryan Kumar <arkumar@webmd.net>
2018-11-28 14:12:55 +0530

    layout changed

--- plain.js
+++ plain.js
@@ -1,14 +1,65 @@
 $(document).ready(function(){
    
-    $("a").addClass( 'btn btn-success' );
+    // $("a").addClass( 'btn btn-success' );
     
-    $("a").hover(function(){
+    // $("a").hover(function(){
         
-        $(this).css("background-color", "green");
-        }, function(){
-        $(this).css("background-color", "#00b33c");
+    //     $(this).css("background-color", "green");
+    //     }, function(){
+    //     $(this).css("background-color", "#00b33c");
         
-    });
+    // });
+
+
 
 
 });
+
+
+/*****************************************************
+ * var template = {
+    _if: '<div class="el_block"><span class="drag_handle">+</span><span class="delete">x</span></div>',
+    _allof: '<div class="el_block"><span class="drag_handle">+</span><span class="delete">x</span></div>',
+    _anyof: '<div class="el_block"><span class="drag_handle">+</span><span class="delete">x</span></div>'
+  };
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+
+  var el = document.createElement('div');
+  el.className = 'el_block';
+  //el.setAttribute('draggable', true);
+  //el.setAttribute('ondragstart', 'drag(event)');
+  //el.setAttribute('ondrop', 'drop(event)');
+  //el.setAttribute('ondragover', 'allowDrop(event)');
+  var el_label = '';
+
+  switch(data){
+         case 'el_if':
+            el_label = '<span class="el_label">if</span>'
+         break;
+      case 'el_allof':
+            el_label = '<span class="el_label">allof</span>'
+         break;
+      case 'el_anyof':
+            el_label = '<span class="el_label">anyof</span>'
+         break;
+   }
+
+  el.innerHTML = el_label + '<span class="buttons"><span class="drag_handle">+</span><span class="delete">x</span></span>';
+
+  //console.log(data);
+  ev.target.appendChild( el );
+console.log(data);
+}
+ * 
+ * ********************************************* */
